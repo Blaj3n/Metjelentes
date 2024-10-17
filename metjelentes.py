02c0863f6bc14f1885e48ae76b9b101eb1fbe53b
Blaj3n <benceulrich@gmail.com>
2024-10-17 16:28:49 +0200

    2. feladat: adott város utolsó mérési adat

--- metjelentes.py
+++ metjelentes.py
@@ -14,3 +14,13 @@ with open("tavirathu13.txt", "r", encoding="utf-8") as file:
 # print(metjelentes)
 print(f"Egysor: {metjelentes[16]}")
 
+feladat(2)
+telepules = input("Adja meg egy település kódját! Település: ")
+ido = []
+for egyelem in metjelentes:
+    if egyelem[0] == telepules:
+        ido = []    # az 'ido' listánkat mindig kiürítem, így a legutolsó 'SM' adatnál az egészet szintúgy kiüríti, és beleteszi az utolsót. Így egy darab elemünk lesz benne.
+        ido.append([egyelem[1], egyelem[2]])
+print(ido)
+print(f"Az utolsó mérési adat a megadott településről {ido[0][0]}:{ido[0][1]}-kor érkezett. ")
+# print(f"Az utolsó mérési adat a megadott településről {ido[-1][0]}:{ido[-1][1]}-kor érkezett. ")
