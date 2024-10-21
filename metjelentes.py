9f54243882c1c8a1e3890aec1ec42ccc7fb85fd8
Blaj3n <benceulrich@gmail.com>
2024-10-21 14:10:14 +0200

    4. és 6. feladat kész

--- metjelentes.py
+++ metjelentes.py
@@ -1,7 +1,7 @@
 # ['BP', 'ora', 'perc', 'szélirány', 'szélerősség',hőmérséklet]
 
 def feladat(szam):
-    print(f"\n{szam}. feladat")
+    print(f"{szam}. feladat")
 
 metjelentes = []
 
@@ -9,32 +9,34 @@ with open("tavirathu13.txt", "r", encoding="utf-8") as file:
     for egysor in file:
         egysor = egysor.strip().split()
         metjelentes.append([str(egysor[0]), str(egysor[1][:2]), str(egysor[1][2:]), str(egysor[2][:3]),
-        str(egysor[2][:3]), str(egysor[2][3:]), int(egysor[3])])
+        str(egysor[2][3:]), int(egysor[3])])
 
 # print(metjelentes)
 print(f"Egysor: {metjelentes[16]}")
 
 feladat(2)
-telepules = input("Adja meg egy település kódját! Település: ")
+telepules = "SM"  #input("Adja meg egy település kódját! Település: ")
 ido = []
 for egyelem in metjelentes:
     if egyelem[0] == telepules:
         ido = []    # az 'ido' listánkat mindig kiürítem, így a legutolsó 'SM' adatnál az egészet szintúgy kiüríti, és beleteszi az utolsót. Így egy darab elemünk lesz benne.
         ido.append([egyelem[1], egyelem[2]])
-print(ido)
+# print(ido)
 print(f"Az utolsó mérési adat a megadott településről {ido[0][0]}:{ido[0][1]}-kor érkezett. ")
 # print(f"Az utolsó mérési adat a megadott településről {ido[-1][0]}:{ido[-1][1]}-kor érkezett. ")
 
+feladat(3) # *
+# homerseklet = []
+# for egyelem in metjelentes:
+#     homerseklet.append(egyelem[-1])
+# print(homerseklet)
 
-feladat(3)
-homerseklet = []
-for egyelem in metjelentes:
-    homerseklet.append(egyelem[-1])
-print(homerseklet)
+homerseklet = [egyelem[-1] for egyelem in metjelentes]
+# print(homerseklet)
 
 legk_hom = min(homerseklet)
 legn_hom = max(homerseklet)
-print(legk_hom, legn_hom)
+# print(legk_hom, legn_hom)
 for egyelem in metjelentes:
     if egyelem[-1] == legk_hom:
         print(f"A legalacsonyabb hőmérséklet: {egyelem[0]} {egyelem[1]}:{egyelem[2]} {legk_hom} fok")
@@ -44,3 +46,35 @@ for egyelem in metjelentes:
     if egyelem[-1] == legn_hom:
         print(f"A legmagasabb hőmérséklet: {egyelem[0]} {egyelem[1]}:{egyelem[2]} {legn_hom} fok")
         break
+
+feladat(4)
+
+szelcsend = False
+for egyelem in metjelentes:
+    if egyelem[3] == "000" and egyelem[4] == "00":
+        szelcsend = True
+        print(f"{egyelem[0]} {egyelem[1]}:{egyelem[2]}")
+
+if not szelcsend: # == nem igaz TEHÁT hamis
+    print("Nem volt szélcsend a mérések idején.")
+
+# not valami -> valami == False
+# valami -> valami == True
+
+
+feladat(6)
+
+telepulesek = []
+
+for egyelem in metjelentes:
+    if egyelem[0] not in telepulesek:
+        telepulesek.append(egyelem[0])
+print(telepulesek)
+
+# list comp.-ben nem lehet 'not in' feltételt írni, hisz a lista folyamatosan íródik.
+for varos in telepulesek:
+    with open(varos + ".txt", "w", encoding="utf-8") as fajl:
+        fajl.write(varos + '\n')
+        for elem in metjelentes:
+            if elem[0] == varos:
+                fajl.write(f"{elem[1]}:{elem[2]} {int(elem[4])*"#"}\n")
